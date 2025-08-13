c79d3670a7a850b7b1c2a1dda00bc17ceebc7a66
Terry Rice <trice7454@gmail.com>
2025-08-13 18:19:35 -0500

    added healthz

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
     "net/http"
 )
 
+func HandleHealthz(writer http.ResponseWriter, request *http.Request)  {
+    writer.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+    writer.WriteHeader(http.StatusOK)
+    writer.Write([]byte("OK"))
+}
+
+
 func main() {
     serveMux := http.NewServeMux()
     server := http.Server {
         Handler: serveMux,
         Addr: ":8080",
     }
-    serveMux.Handle("/", http.FileServer(http.Dir(".")))
+    serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    serveMux.HandleFunc("/healthz", HandleHealthz)
     server.ListenAndServe()
 }
