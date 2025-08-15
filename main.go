38c7d45e35bf2668d489d47c8e60a217a8f16310
Terry Rice <trice7454@gmail.com>
2025-08-14 19:51:35 -0500

    Middleware

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg * apiConfig) GetHits(writer http.ResponseWriter, request *http.Request) {
+    writer.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+    writer.WriteHeader(http.StatusOK)
+    hits := cfg.fileserverHits.Load()
+    message := fmt.Sprintf("Hits: %v", hits)
+    writer.Write([]byte(message))
+}
+
+func (cfg * apiConfig) resetHits(writer http.ResponseWriter, request *http.Request) {
+    cfg.fileserverHits.Store(0)
+}
 
 func HandleHealthz(writer http.ResponseWriter, request *http.Request)  {
     writer.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
@@ -12,12 +37,17 @@ func HandleHealthz(writer http.ResponseWriter, request *http.Request)  {
 
 
 func main() {
+    theCounter := apiConfig{}
     serveMux := http.NewServeMux()
     server := http.Server {
         Handler: serveMux,
         Addr: ":8080",
     }
-    serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    serveMux.Handle("/app/", http.StripPrefix("/app",
+        theCounter.MiddlewareMetricsInc(http.FileServer(http.Dir(".")))))
     serveMux.HandleFunc("/healthz", HandleHealthz)
+    serveMux.HandleFunc("/metrics", theCounter.GetHits)
+    serveMux.HandleFunc("/reset", theCounter.resetHits)
+
     server.ListenAndServe()
 }
